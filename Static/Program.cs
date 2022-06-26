e9c582e4e94a8b64ee243eac8ff8f3b5dcaa61fb
Brittanie Campos <brittanie.angelique@gmail.com>
2022-06-26 12:32:53 -0700

    Add project files.

---
+++ Static/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // static = modifier to declare a static member, which belongs to the class itself
+        //          rather than to any specific object
+
+        Car car1 = new Car("Mustang");
+        Car car2 = new Car("Corvette");
+
+        Console.WriteLine(Car.numberOfCars);
+
+        Car.StartRace();
+
+        Console.ReadKey();
+    }
+}
+
+class Car
+{
+    String model;
+    public static int numberOfCars;
+
+    public Car(String model)
+    {
+        this.model = model;
+        numberOfCars++;
+    }
+
+    public static void StartRace()
+    {
+        Console.WriteLine("The race has begun!");
+    }
+
+}
